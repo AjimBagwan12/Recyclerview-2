35d8ff3fd8774372f6f29c2ad3f762306ffdc862
ajim <ajeembagwan313@gmail.com>
2023-09-04 21:13:01 +0530

    Recyclerview-2

---
+++ app/src/main/java/com/example/recycleview_2/CitiesAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.recycleview_2
+
+import android.util.Log
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class CitiesAdapter(
+    private val cities : ArrayList<String>
+) : RecyclerView.Adapter<CitiesAdapter.CityViewHolder>() {
+
+    inner class CityViewHolder(val txtCity : TextView) : ViewHolder(txtCity) {
+       /* init {
+            //Way 3 - Perfecto
+            txtCity.setOnClickListener {
+                Toast.makeText(it.context, "${cities[adapterPosition]} $adapterPosition", Toast.LENGTH_SHORT).show()
+            }
+        }
+        
+        */
+    }
+
+
+
+    override fun getItemCount(): Int {
+        Log.e("bit", "getItemCount")
+        return cities.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
+        Log.e("bit", "onCreateViewHolder")
+
+        val txtCity = TextView(parent.context)
+        txtCity.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT
+        )
+        txtCity.textSize = 25f
+        txtCity.setPadding(40, 60, 20, 60)
+
+        //way 1 Handling click events //not good
+        /*txtCity.setOnClickListener {
+            Toast.makeText(it.context, "City Clicked", Toast.LENGTH_SHORT).show()
+        }*/
+
+        return CityViewHolder(txtCity)
+
+    }
+
+    override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
+        Log.e("bit", "onBindViewHolder : $position")
+        holder.txtCity.text = "${cities[position]}"
+
+        //way 2 -- works, however it is still not good
+        holder.txtCity.setOnClickListener {
+            Toast.makeText(it.context, cities[position], Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/recycleview_2/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.recycleview_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerCities : RecyclerView
+    private val cities = ArrayList<String>()
+    private lateinit var citiesAdapter: CitiesAdapter
+    private lateinit var edtCity : EditText
+    private lateinit var btnAddCity : Button
+    private lateinit var btnRemoveCity : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        initDataSource()
+        initViews()
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        btnAddCity.setOnClickListener {
+            cities.add(edtCity.text.toString())
+            //citiesAdapter.notifyDataSetChanged()
+            citiesAdapter.notifyItemInserted(cities.size-1)
+        }
+
+        btnRemoveCity.setOnClickListener {
+
+            val city = edtCity.text.toString()
+            if(cities.contains(city)) {
+                val indexOfCity = cities.indexOf(city)
+                cities.remove(city)
+                //citiesAdapter.notifyDataSetChanged()
+                citiesAdapter.notifyItemRemoved(indexOfCity)
+            }
+        }
+    }
+
+    private fun initDataSource() {
+        cities.add("Pune")
+        cities.add("Mumbai")
+        cities.add("Chennai")
+    }
+
+    private fun initViews() {
+
+        edtCity = findViewById(R.id.edtCity)
+        btnAddCity = findViewById(R.id.btnAddCity)
+        btnRemoveCity = findViewById(R.id.btnRemoveCity)
+
+        recyclerCities = findViewById(R.id.recyclerCities)
+        citiesAdapter = CitiesAdapter(cities)
+        recyclerCities.adapter = citiesAdapter
+        recyclerCities.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+}
